e9fc2f5c437fce0f35bf9704bc47cf5edaca4626
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2025-03-24 15:08:47 +0100

    first commit for new rapid client

---
+++ app/(auth)/account-setup/page.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../../assets/logo.png';
+
+const AccountSetup = () => {
+  return (
+    <div className='bg-[#DCE8F5] min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl w-full mx-4 py-8 px-18 bg-[#F5F7FA] flex flex-col items-center justify-center gap-6 shadow-md rounded-4xl'>
+        <Image src={logo} alt='rapidClient logo' width={36} height={56} />
+        <p className='text-[#26313D] text-2xl font-bold text-center'>
+          Finish your account setup
+        </p>
+
+        <div className='flex flex-col gap-4 w-full'>
+          <p className='text-[#3C4B5C] font-bold text-xl'>Provide your names</p>
+          <input
+            type='text'
+            placeholder='First name'
+            className='bg-[#DCE8F5] p-4 w-full rounded-[28px] outline-none'
+          />
+          <input
+            type='text'
+            placeholder='Middle name'
+            className='bg-[#DCE8F5] p-4 w-full rounded-[28px] outline-none'
+          />
+          <input
+            type='text'
+            placeholder='Surname'
+            className='bg-[#DCE8F5] p-4 w-full rounded-[28px] outline-none'
+          />
+        </div>
+
+        <div className='flex flex-col gap-4 w-full'>
+          <p className='text-[#3C4B5C] font-bold text-xl'>Join organization</p>
+          <input
+            type='text'
+            placeholder='Enter invitation code'
+            className='bg-[#DCE8F5] p-4 w-full rounded-[28px] outline-none'
+          />
+        </div>
+
+        <div className='flex flex-col gap-4 w-full'>
+          <p className='text-[#3C4B5C] font-bold text-xl'>
+            Create your password
+          </p>
+          <input
+            type='password'
+            placeholder='Password'
+            className='bg-[#DCE8F5] p-4 w-full rounded-[28px] outline-none'
+          />
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            className='bg-[#DCE8F5] p-4 w-full rounded-[28px] outline-none'
+          />
+        </div>
+        <button className='bg-[#4188D9] p-4 w-full text-white rounded-[28px] text-lg font-bold cursor-pointer'>
+          Create account
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AccountSetup;
---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from '../../assets/logo.png';
+import GoogleIcon from '@/app/assets/googleIcon';
+import Link from 'next/link';
+
+const Login = () => {
+  return (
+    <div className='bg-[#DCE8F5] min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl w-full mx-4 py-8 px-18 bg-[#F5F7FA] flex flex-col items-center justify-center gap-6 shadow-md rounded-4xl'>
+        <Image src={logo} alt='rapidClient logo' width={36} height={56} />
+        <p className='text-[#151C24] text-2xl font-bold text-center'>
+          Log in to your account
+        </p>
+        <input
+          type='text'
+          placeholder='Email'
+          className='bg-[#DCE8F5] p-4 w-full rounded-[28px] outline-none'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          className='bg-[#DCE8F5] p-4 w-full rounded-[28px] outline-none'
+        />
+        <button className='bg-[#4188D9] p-4 w-full text-white rounded-[28px] text-lg font-bold cursor-pointer'>
+          Log in
+        </button>
+
+        <div className='flex items-center w-full my-2'>
+          <div className='flex-grow h-px bg-[#C5D4E5]'></div>
+          <p className='mx-4 text-[#738599] text-xl'>OR</p>
+          <div className='flex-grow h-px bg-[#C5D4E5]'></div>
+        </div>
+
+        <button className='border border-[#C5D4E5] p-4 w-full rounded-[28px] flex items-center justify-center gap-2 text-[#738599] cursor-pointer font-bold'>
+          <GoogleIcon />
+          Sign in with Google
+        </button>
+
+        <p className='text-[#738599] font-bold'>
+          Already have an account?{' '}
+          <span className='text-[#4188D9] cursor-pointer underline'>
+            <Link href={'/register'}>Create one</Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
